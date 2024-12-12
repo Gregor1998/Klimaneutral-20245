0c61edb7a27caf18a0c68ca172f35875ae02609a
Joris.Mende <joris.mende@qbeyond.de>
2024-12-12 22:18:39 +0100

    Erweiterung durch Flexible Kraftwerken

--- utils/calcDifference.py
+++ utils/calcDifference.py
@@ -19,7 +19,7 @@ def differenceBetweenDataframes(df1, df2):
         return None
     
 
-def StorageIntegration(difference_df, storage_capacity):
+def StorageIntegration(difference_df, storage_capacity, flexipowerplant_power):
     """
     Integrates storage based on the difference dataframe, storage capacity, and threshold.
     Parameters:
@@ -30,47 +30,71 @@ def StorageIntegration(difference_df, storage_capacity):
     pd.DataFrame: DataFrame with 'Datum', 'Differenz', 'Speicher', and 'Netz' columns, where 'Speicher' represents the storage values and 'Netz' represents the net energy flow.
     """
     storage = 0
-    capacity = storage_capacity * 10**6
+    flexipowerplant = 0
+    battery_capacity = storage_capacity * 10**6 # in kWh
+    #pumpstorage_capacity = pumpstorage_capacity * 10**6
+    flexipowerplant_power = flexipowerplant_power * 10**6 # in kW
+    flexipowerplant_capacity = flexipowerplant_power * (15/60) # in kWh
+
+
     storage_df = pd.DataFrame()
     storage_df['Datum'] = difference_df['Datum']
-    storage_df['Differenz'] = difference_df['Differenz']
-    storage_df['Kapazität'] = 0.0
-    storage_df['Laden/Einspeisen'] = 0.0
+    storage_df['Differenz in kWh'] = difference_df['Differenz']
+    storage_df['Kapazität in kWh'] = 0.0
+    storage_df['Laden/Einspeisen in kWh'] = 0.0
+
+    """pumpstorage_df = pd.DataFrame()
+    pumpstorage_df['Datum'] = difference_df['Datum']
+    pumpstorage_df['Einspeisung in kWh'] = 0.0"""
+
+    flexipowerplant_df = pd.DataFrame()
+    flexipowerplant_df['Datum'] = difference_df['Datum']
+    flexipowerplant_df['Kapazität in kWh'] = flexipowerplant_capacity
+    flexipowerplant_df['Restkapazität in kWh '] = flexipowerplant_capacity
+    flexipowerplant_df['Einspeisung in kWh'] = 0.0
 
     for i in range(len(difference_df)):
         diff = difference_df.loc[i, 'Differenz']
         
         if diff < 0:  # Überschussenergie
-            if storage < capacity:
-                if storage + abs(diff) > capacity:
-                    storage = capacity
-                    storage_df.loc[i, 'Kapazität'] = storage
-                    storage_df.loc[i, 'Laden/Einspeisen'] = (-1)*diff
+            if storage < battery_capacity:
+                if storage + abs(diff) > battery_capacity:
+                    storage = battery_capacity
+                    storage_df.loc[i, 'Kapazität in kWh'] = storage
+                    storage_df.loc[i, 'Laden/Einspeisen in kWh'] = (-1)*diff
                 else:
                     storage += abs(diff)
-                    storage_df.loc[i, 'Kapazität'] = storage
-                    storage_df.loc[i, 'Laden/Einspeisen'] = (-1)*diff
+                    storage_df.loc[i, 'Kapazität in kWh'] = storage
+                    storage_df.loc[i, 'Laden/Einspeisen in kWh'] = (-1)*diff
             else:
-                storage_df.loc[i, 'Kapazität'] = storage
-                storage_df.loc[i, 'Laden/Einspeisen'] = 0
+                storage_df.loc[i, 'Kapazität in kWh'] = storage
+                storage_df.loc[i, 'Laden/Einspeisen in kWh'] = 0
         else:  # Energiedefizit
             if storage > 0:
                 if storage - diff < 0:
+                    storage_df.loc[i, 'Kapazität in kWh'] = storage
+                    storage_df.loc[i, 'Laden/Einspeisen in kWh'] = (-1)*storage
+                    flexipowerplant_df.loc[i, 'Einspeisung in kWh'] = storage - diff
+                    flexipowerplant_df.loc[i, 'Restkapazität in kWh'] = flexipowerplant_capacity - abs(storage - diff)
                     storage = 0
-                    storage_df.loc[i, 'Kapazität'] = storage
-                    storage_df.loc[i, 'Laden/Einspeisen'] = (-1)*storage
                 else :
                     storage -= diff
-                    storage_df.loc[i, 'Kapazität'] = storage
-                    storage_df.loc[i, 'Laden/Einspeisen'] = (-1)*diff
+                    storage_df.loc[i, 'Kapazität in kWh'] = storage
+                    storage_df.loc[i, 'Laden/Einspeisen in kWh'] = (-1)*diff
             else:
-                storage_df.loc[i, 'Kapazität'] = 0
-                storage_df.loc[i, 'Laden/Einspeisen'] = 0
-    return storage_df
+                storage_df.loc[i, 'Kapazität in kWh'] = 0
+                storage_df.loc[i, 'Laden/Einspeisen in kWh'] = 0
+                if flexipowerplant_capacity - abs(diff) > 0:
+                    flexipowerplant_df.loc[i, 'Einspeisung in kWh'] = (-1)*diff
+                    flexipowerplant_df.loc[i, 'Restkapazität in kWh'] = flexipowerplant_capacity - abs(storage - diff)
+                else: 
+                    flexipowerplant_df.loc[i, 'Einspeisung in kWh'] = flexipowerplant_capacity
+                    flexipowerplant_df.loc[i, 'Restkapazität in kWh'] = 0
+    return storage_df, flexipowerplant_df
 
 """def StorageIntegration(difference_df, storage_capacity):
     
-    Integrates storage based on the difference dataframe, storage capacity, and threshold.
+    Integrates storage based on the difference dataframe, storage battery_capacity, and threshold.
     Parameters:
     difference_df (pd.DataFrame): DataFrame containing 'Datum' and 'Differenz' columns.
     storage_capacity (int): Maximum storage capacity.
