3de01ec0ca31af96a5c15a2809056f71b026ecfe
Emily <emily.klemt@web.de>
2024-12-13 23:16:24 +0100

    start integrating§

--- utils/calculateConsumption.py
+++ utils/calculateConsumption.py
@@ -1,20 +1,34 @@
+import pandas as pd
 from utils.read_CSV import getData
 from utils.combineDataFrames import combineDataFrames
-from utils.extraploation_class import Extrapolation
+from utils.extraploation_class import Extrapolation, Extrapolation_Consumption
 
 def calculateConsumption(consumption_development_rate): 
-    directory_yearly_consumption = getData("Verbrauch")
+    directory_yearly_consumption = getData("Consumption")
 
-    for year in range(2024,2031):
+    for year in range(2024,2025):
         prev_year_df =directory_yearly_consumption.get(year-1).copy()    #Kopie des Dataframe des letzten Jahres
         extrapolated_data = Extrapolation(prev_year_df, year, None, None, None, consumption_development_rate)        #Erstellung eines neuen Objekts, mit einem DataFrame
         directory_yearly_consumption[extrapolated_data.year]= extrapolated_data.df   #DataFrame in das Erzeugungsverzeichnis gespeichert wird
 
-    #consumption_extrapolation = combineDataFrames(directory_yearly_consumption,2023,2031)
     
     return directory_yearly_consumption
 
 
 
+
 def getConsumptionYear(year, data_df):
     return data_df.get(year)
+
+
+def calculateConsumption_lastprofile(consumption_development_rate, lastprofile_dict): 
+   
+    directory_yearly_consumption = getData("Consumption")
+
+    for year in range(2024,2025):
+        prev_year_df =directory_yearly_consumption.get(year-1).copy()    #Kopie des Dataframe des letzten Jahres
+        extrapolated_data = Extrapolation_Consumption(prev_year_df, year, None, None, None, consumption_development_rate, lastprofile_dict)        #Erstellung eines neuen Objekts, mit einem DataFrame
+        directory_yearly_consumption[extrapolated_data.year]= extrapolated_data.df   #DataFrame in das Erzeugungsverzeichnis gespeichert wird
+
+    
+    return directory_yearly_consumption
--- utils/read_CSV.py
+++ utils/read_CSV.py
@@ -47,7 +47,7 @@ def read_SMARD_data(path, mode):
         df.reset_index(drop=True, inplace=True)
 
 
-    elif mode == "Comsumption":
+    elif mode == "Consumption":
         #Umbenennung der Spalten
         df.rename(columns= {
         "Datum von":"Datum",
@@ -122,16 +122,16 @@ def read_SMARD_data(path, mode):
 def getData(mode):
     dataFrames = {}
 
-    if mode == "Comsumption":
+    if mode == "Consumption":
          # Dictionary für die df für jedes Jahr
-        path_var = "CSV/Comsumption/" #Pfad auf den Ordner, um später durch die Datein zu navigieren
+        path_var = "CSV/Consumption/" #Pfad auf den Ordner, um später durch die Datein zu navigieren
 
         #Schleife für die Jahre 2015-2023 und Einlesen der Datei
         for year in range(2023,2024): # hier könnte man später sich die Jahre auch vom User geben lassen, welche Jahre er gerne eingelesen haben möchte
             #Dateipfad für das entsprechende Jahr
             file_path = os.path.join(path_var, f"Realisierter_Stromverbrauch_{year}01010000_{year+1}01010000_Viertelstunde.csv")
             if os.path.exists(file_path):   #Falls dieser zusammengesetze Pfad existiert,...
-                dataFrames[year] = read_SMARD_data(file_path, "Comsumption")   #... soll dieser eingelesen werden
+                dataFrames[year] = read_SMARD_data(file_path, "Consumption")   #... soll dieser eingelesen werden
                 print(f"Data für {year} loaded succsessfully.")
             else:
                 print(f"File for {year} not found at path: {file_path}") #... anstonsten nicht
@@ -168,7 +168,7 @@ def getData(mode):
             df_heatpump = read_SMARD_data(file_path, "Heatpump")
 
     else:
-        print("Mode not found")
+        print("Mode not found :(")
 
     
     if mode == "Heatpump":
