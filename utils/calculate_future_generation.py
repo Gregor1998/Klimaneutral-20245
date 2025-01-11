b2e63a4b1bdd5605ae4671b6ce191f5bbf755bd8
AaronKoopmann <wzw655@haw-hamburg.de>
2025-01-11 17:07:35 +0100

    Anlegung Capex Datei und anpassung von ein paat variablen, die später mitgegeben werden sollen

--- utils/calculate_future_generation.py
+++ utils/calculate_future_generation.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from utils.addTimePerformance import addTimePerformance
-from szenarioDefinition.szenario import *
 
-def calculate_future_generation(case_PV, case_WindOnshore, case_WindOffshore, dataFrame_performance, dataFrame_generation_start_year):
+
+def calculate_future_generation(case_PV, case_WindOnshore, case_WindOffshore, dataFrame_performance, dataFrame_generation_start_year, netzverluste, start_year, end_year):
     # Erstellung eines leeren Dictionaries für die Generation
     directoryGeneration = {}
 
@@ -20,10 +20,10 @@ def calculate_future_generation(case_PV, case_WindOnshore, case_WindOffshore, da
     df_Onshore = pd.read_csv(filepath_Onshore)
     df_Offshore = pd.read_csv(filepath_Offshore)
 
-    # Alle Daten ab 2024 sind von Interesse
-    df_filtered_PV = df_PV[df_PV['year'] >= 2024].reset_index(drop=True)
-    df_filtered_Onshore = df_Onshore[df_Onshore['year'] >= 2024].reset_index(drop=True)
-    df_filtered_Offshore = df_Offshore[df_Offshore['year'] >= 2024].reset_index(drop=True)
+    # Alle Daten ab start_year sind von Interesse
+    df_filtered_PV = df_PV[df_PV['year'] >= start_year].reset_index(drop=True)
+    df_filtered_Onshore = df_Onshore[df_Onshore['year'] >= start_year].reset_index(drop=True)
+    df_filtered_Offshore = df_Offshore[df_Offshore['year'] >= start_year].reset_index(drop=True)
 
     # Zusammenführen der Daten
     df_combined = pd.concat([
@@ -37,8 +37,8 @@ def calculate_future_generation(case_PV, case_WindOnshore, case_WindOffshore, da
     # Umwandlung in ein Dictionary
     directoryInstalled = df_combined.set_index('Jahr').to_dict(orient='index')
 
-    # Schleife für die Jahre 2024 bis 2030
-    for year in range(2024, 2031):
+    # Schleife für die Jahre zwischen start_year und end_year
+    for year in range(start_year, end_year + 1):
         if year + 1 in directoryInstalled:  # Falls ein Folgejahr existiert
             dayly_expansion_rate_PV = (directoryInstalled[year + 1]['Photovoltaik'] - directoryInstalled[year]['Photovoltaik']) / 365
             dayly_expansion_rate_Onshore = (directoryInstalled[year + 1]['Wind Onshore'] - directoryInstalled[year]['Wind Onshore']) / 365
@@ -66,7 +66,7 @@ def calculate_future_generation(case_PV, case_WindOnshore, case_WindOffshore, da
             Onshore_generation.extend(df_performance['Wind Onshore'].iloc[start_index:end_index] * daily_Onshore)
             Offshore_generation.extend(df_performance['Wind Offshore'].iloc[start_index:end_index] * daily_Offshore)
 
-        # Erstellung eines DataFrames für das Jahr
+        # Erstellung eines DataFrames für das Jahr, dabei bleiben die Werte für Wasserkraft, Biomasse und Sonstige Erneuerbare gleich
         combined_generation = pd.DataFrame({
             'Photovoltaik': PV_generation,
             'Wind Onshore': Onshore_generation,
