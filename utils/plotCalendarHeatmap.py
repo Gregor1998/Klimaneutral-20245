8d20fc74e400bfda188e256cb415981cbba12d0b
Emily <emily.klemt@web.de>
2025-01-20 14:04:53 +0100

    UPDATE: plotCalendarHeatmap

--- utils/plotCalendarHeatmap.py
+++ utils/plotCalendarHeatmap.py
@@ -5,22 +5,19 @@ import matplotlib.pyplot as plt # type: ignore
 # Heatmap über Monate und Tage
 def plotCalendarHeatmap(df_list, title_list, colName, linewidths=0.01):
     # Determine the global min and max values for the color scale
-    vmin = float('inf')
-    vmax = float('-inf')
-    for df in df_list:
-        heatmap_data = df.pivot_table(index='Year Month', columns='Day', values=colName, aggfunc=np.sum)
-        vmin = min(vmin, heatmap_data.min().min())
-        vmax = max(vmax, heatmap_data.max().max())
-
+    vmin = -1e6
+    vmax = 2.5e6 
+    
     # Plot each heatmap with the same color scale
     for df, title in zip(df_list, title_list):
         heatmap_data = df.pivot_table(index='Year Month', columns='Day', values=colName, aggfunc=np.sum)
 
         plt.figure(figsize=(20, 8))
         
-
+        # Use a diverging color palette
         cmap = sns.diverging_palette(200, 30, as_cmap=True)
 
+        # Create the heatmap with the same color scale for all plots
         sns.heatmap(heatmap_data, cmap=cmap, annot=False, linewidths=linewidths, cbar=True, xticklabels=1, cbar_kws={'label': 'MWh'}, vmin=vmin, vmax=vmax)
 
         plt.title(title)
